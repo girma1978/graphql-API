63803dff5c83a37c5088f1a49a0b02bee3e38272
Lakulength@1717 <girm2002@gmail.com>
2025-04-08 23:56:56 -0400

    mongodb server dependecy added

--- server/render-build.sh
+++ server/render-build.sh
@@ -5,11 +5,23 @@ npm install --legacy-peer-deps
 # Build the TypeScript code
 npm run build
 
-# Create the directory structure if needed
+# Create Apollo utils directory structure
 mkdir -p node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist
-
-# Only create the file if it doesn't exist
 if [ ! -f node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist/index.js ]; then
   echo 'function isNodeLike() { return true; } module.exports = { isNodeLike };' > node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist/index.js
   echo '{"name":"@apollo/utils.isnodelike","version":"1.0.0","main":"dist/index.js"}' > node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/package.json
+fi
+
+# Create saslprep directory structure
+mkdir -p node_modules/@mongodb-js/saslprep/dist
+if [ ! -f node_modules/@mongodb-js/saslprep/dist/node.js ]; then
+  echo 'function saslprep(str) { return str; } module.exports = saslprep;' > node_modules/@mongodb-js/saslprep/dist/node.js
+  echo '{"name":"@mongodb-js/saslprep","version":"1.1.0","main":"dist/node.js"}' > node_modules/@mongodb-js/saslprep/dist/package.json
+fi
+
+# Also handle the nested path if needed
+mkdir -p node_modules/mongoose/node_modules/mongodb/node_modules/@mongodb-js/saslprep/dist
+if [ ! -f node_modules/mongoose/node_modules/mongodb/node_modules/@mongodb-js/saslprep/dist/node.js ]; then
+  echo 'function saslprep(str) { return str; } module.exports = saslprep;' > node_modules/mongoose/node_modules/mongodb/node_modules/@mongodb-js/saslprep/dist/node.js
+  echo '{"name":"@mongodb-js/saslprep","version":"1.1.0","main":"dist/node.js"}' > node_modules/mongoose/node_modules/mongodb/node_modules/@mongodb-js/saslprep/dist/package.json
 fi
