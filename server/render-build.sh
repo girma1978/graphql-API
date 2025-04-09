f311aca0090a5265f89dd2748daccf92a76305c0
Lakulength@1717 <girm2002@gmail.com>
2025-04-08 23:52:38 -0400

    mongodb dependency dadded after modification of json files

--- server/render-build.sh
+++ server/render-build.sh
@@ -10,6 +10,6 @@ mkdir -p node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist
 
 # Only create the file if it doesn't exist
 if [ ! -f node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist/index.js ]; then
-  echo 'export function isNodeLike() { return true; }' > node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist/index.js
+  echo 'function isNodeLike() { return true; } module.exports = { isNodeLike };' > node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/dist/index.js
   echo '{"name":"@apollo/utils.isnodelike","version":"1.0.0","main":"dist/index.js"}' > node_modules/@apollo/server/node_modules/@apollo/utils.isnodelike/package.json
 fi
