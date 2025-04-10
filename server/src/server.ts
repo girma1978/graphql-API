66f2bf85a8dc3c4cd470d783e7f36784bccdf55d
Lakulength@1717 <girm2002@gmail.com>
2025-04-10 00:38:23 -0400

    server json file modified again15

--- server/src/server.ts
+++ server/src/server.ts
@@ -48,9 +48,10 @@ const startApolloServer = async () => {
   }));
   
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    // Updated path to match Render's file structure
+    app.use(express.static(path.join(__dirname, 'client/dist')));
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, 'client/dist/index.html'));
     });
   }
   
