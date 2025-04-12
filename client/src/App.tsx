a45d7108b656db2c74f7ad9869429fefbf392f93
Lakulength@1717 <girm2002@gmail.com>
2025-04-11 22:05:04 -0400

    App.tsx modified

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,3 @@
-
-
-// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
-
-// const cache = new InMemoryCache();
-
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache,
-// });
-
-// const App = () => {
-//   return (
-//     <ApolloProvider client={client}>
-//       <Router> {/* Wrap the app in a single Router */}
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<SearchBooks />} />
-//           <Route path="/saved" element={<SavedBooks />} />
-//         </Routes>
-//       </Router>
-//     </ApolloProvider>
-//   );
-// };
-
-// export default App;
-
-
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, ApolloLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -38,8 +6,13 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Auth from './utils/auth'; // Ensure the path to your Auth utility is correct
 
+// Create a conditional URL based on the environment
+const apiUrl = import.meta.env.PROD 
+  ? 'https://graphql-api-8rbg.onrender.com/graphql' 
+  : '/graphql';
+
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: apiUrl,
 });
 
 const authLink = setContext((_, { headers }) => {
